ba192b73488af8334d8f6761f5ef2cbfc934c0a6
duynpce <a23.thvl03@gmail.com>
2026-05-10 08:46:32 +0700

    test CI (should fail)

---
+++ src/test/java/com/example/worker_observer_system/temp/TestCI.java
@@ -0,0 +1,12 @@
+package com.example.worker_observer_system.temp;
+
+import com.example.worker_observer_system.common.UnitTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCI extends UnitTest {
+    @Test
+    public void testCI_Fail() {
+        Assertions.assertTrue(false);
+    }
+}
