3d47a6a11d342188673dcd1c5c5f43d5ad674633
duynpce <a23.thvl03@gmail.com>
2026-05-10 14:43:03 +0700

    feat: add security layer
    - CustomUserDetailsService  for Spring boot's login api
    - SecurityConfig  for api protection

---
+++ src/main/java/com/example/worker_observer_system/common/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.worker_observer_system.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        //public endpoints
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/accounts").permitAll()
+                        //authenticated endpoints
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
+
